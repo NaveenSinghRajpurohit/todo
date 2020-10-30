a66e359a55972c2281bcf0510cc1202cfbda99d1
naveen <naveenrajpurohit786@gmail.com>
2020-10-30 08:10:49 +0530

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <input type="text" class="todu-input" placeholder="what needs to be done">
+<p>todo list goes here</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.todo-input {
+    width: 100%;
+    padding: 10px 10px;
+    font-size: 18px;
+    margin-bottom: 16px;
+  
+}
+</style>
